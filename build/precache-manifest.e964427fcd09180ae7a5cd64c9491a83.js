ca65c795470743ca578a35d5acd613511c47667e
Nico Reed <nr830411@gmail.com>
2020-04-04 12:41:10 -0400

    (added build)

---
+++ build/precache-manifest.e964427fcd09180ae7a5cd64c9491a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83bd7788edf0eb7ed44086192bfbd90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e1650625c044313935",
+    "url": "/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "05e1650625c044313935",
+    "url": "/static/js/main.96698a82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
